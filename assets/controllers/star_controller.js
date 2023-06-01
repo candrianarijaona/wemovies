c392bd22f0064148cd416c863ff73d13e0eedf33
Claude Andrianarijaona <samuelson.claude@gmail.com>
2023-06-01 11:53:38 +0200

    work on stars suite (js)

--- assets/controllers/star_controller.js
+++ assets/controllers/star_controller.js
@@ -4,10 +4,22 @@ export default class extends Controller {
     static targets = ["average"]
 
     connect() {
-        const starCount = Math.round(parseInt(this.averageTarget.value) / 2);
+        const movieAverage = this.averageTarget.value / 2;
+        const fullStars = Math.floor(movieAverage);
+        const halfStar = movieAverage > fullStars;
+        const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
         let text = '';
-        for (let i = 1; i <= 5; i++) {
-            text += `<div class="${i <= starCount ? 'fill' : 'blank'} mr-1" ><i class="fa-solid fa-star"></i></div>`;
+        for (let i = 1; i <= fullStars; i++) {
+            text += `<div class="fill mr-1" ><i class="fa-solid fa-star"></i></div>`;
+        }
+
+        if (halfStar) {
+            text += `<div class="fill mr-1" ><i class="fa-solid fa-star-half-stroke"></i></div>`;
+        }
+
+        for (let i = 1; i <= emptyStars; i++) {
+            text += `<div class="blank mr-1" ><i class="fa-solid fa-star"></i></div>`;
         }
 
         this.element.innerHTML = text;
